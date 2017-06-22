c7ef989e454ddf66773c9b6c2b83e958ab4a09f8
Lokyee Au <lokyee.au@gmail.com>
2017-06-22 10:32:13 -0700

    Finish web caesar

--- main.py
+++ main.py
@@ -4,48 +4,49 @@ from caesar import rotate_string
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-Form = """
+form = """
 <!DOCTYPE html>
 
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
-        <form action"/" method = "post>
-        <label for="form">
-            <input type="text name="rot">0</input>
-            <textarea name="text">
+        <form method = "post">
+            <label for="rot">Rotate by:</label>
+            <input type="text name="rot" value="0">
+            <br>
+            <textarea type="0" name="text">{0}</textarea>
             <input type="submit" value="Submit"/>
-        </label>
-    </form>
+        </form>
     </body>
 </html>
 """
 
-@app.route("/", methods = ['POST'])
+@app.route("/", methods=['POST'])
 def index():
-    return Form
+    return form.format('')
 
-@app.route("/", methods = ['POST'])
+@app.route("/", methods=['POST'])
 def encrypt():
     rot = request.form['rot']
     text = request.form['text']
     text = int(text)
-    return rotate_string(text, rot)
+    rotated_string = "<h1>" + rotate_string(text, rot) + "</h1>"
+    return form.format(rotated_string)
 
 app.run()
